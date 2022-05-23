80bd36853cbbb1294c777739226db898ae2ece62
sdott3 <sdotthomas347@gmail.com>
2022-05-23 10:01:56 -0400

    task can be added to to do list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,27 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    // create div to hold task info and add to list item
+    var taskinfoE1 = document.createElement("div");
+    // give it a class name
+    taskinfoE1.className = "task-info";
+    
+    // add HTML content to div
+    taskinfoE1.innerHTML= "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.textContent = taskNameInput;
+
+    // add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
